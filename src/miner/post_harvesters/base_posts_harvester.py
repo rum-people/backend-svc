d7f077dd53bd90cd445affb356cbd5cd7380d269
statnik <ext-mykhailo.statnik@here.com>
2024-02-10 15:14:06 +0200

    feat: added reddit post harverster, get popular posts functional

--- src/miner/post_harvesters/base_posts_harvester.py
+++ src/miner/post_harvesters/base_posts_harvester.py
@@ -1,3 +1,3 @@
-class Miner:
-    def get_posts(days: int, quantity: int=-1) -> list:
+class BasePostsHarvester:
+    def get_posts(self, days: int, quantity: int=-1) -> list:
         raise NotImplementedError('This method must be implemented in the derived class.')
