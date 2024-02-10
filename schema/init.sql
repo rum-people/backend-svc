2571eff79aefeae6235318e4afa88727250db752
statnik <ext-mykhailo.statnik@here.com>
2024-02-10 20:34:53 +0200

    fix: corrected sql schema

--- schema/init.sql
+++ schema/init.sql
@@ -1,20 +1,16 @@
--- Create the database
-CREATE DATABASE IF NOT EXISTS backend-svc;
-USE backend-svc;
-
 -- Create the 'posts' table
 CREATE TABLE IF NOT EXISTS posts (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     provider_name VARCHAR(255),
-    text TEXT NOT NULL,
-    timestamp DATETIME,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP,
     emotional_trait VARCHAR(255),
     link TEXT
 );
 
 -- Create the 'post_keywords' table
 CREATE TABLE IF NOT EXISTS post_keywords (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     post_id INT,
     keyword TEXT,
     FOREIGN KEY (post_id) REFERENCES posts(id)
